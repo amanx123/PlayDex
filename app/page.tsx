4305c60eaf98c9ac820549455f3af22c79115f47
aman <amanagarwalx123@gmail.com>
2023-12-13 12:37:08 +0530

    maintenance

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         ))}
       </main> */}
       {/* Maintenance */}
-      <div className='flex items-center justify-center text-4xl font-bold mx-auto h-screen'>
+      <div className='flex items-center justify-center text-4xl font-bold mx-auto h-screen flex-wrap px-8'>
         Under Maintenance , will launch soon.
       </div>
 
